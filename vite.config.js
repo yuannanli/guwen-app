523005100e50b24856de541180729acddca438c1
yuannanli <136249829+yuannanli@users.noreply.github.com>
2026-04-22 14:49:55 +0800

    vite.config.js: clean build config for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
+      "/api": "http://localhost:5000"
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js"
       }
     }
   }
